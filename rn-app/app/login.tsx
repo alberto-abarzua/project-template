6a14ea690f11dcb65dd005c3cb8dc90d8267f67e
alberto-abarzua <uintuser@pm.me>
2024-03-21 21:13:51 -0300

    chore: update login styling

--- rn-app/app/login.tsx
+++ rn-app/app/login.tsx
@@ -7,12 +7,14 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { userSliceActions } from '@/redux/slices/userSlice';
 import supabase from '@/utils/supabase/client';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
-
-const StyledView = View;
-const StyledText = Text;
-const StyledTextInput = TextInput;
-const StyledTouchableOpacity = TouchableOpacity;
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    Keyboard,
+} from 'react-native';
 
 const formSchema = z.object({
     email: z.string().email(),
@@ -58,63 +60,67 @@ const LoginForm = () => {
     return (
         <>
             <Stack.Screen options={{ title: 'Login' }} />
-            <StyledView className="flex h-full w-full flex-col items-center justify-center">
-                <StyledView className=" w-full px-4">
-                    <Controller
-                        control={control}
-                        name="email"
-                        render={({ field: { onChange, onBlur, value } }) => (
-                            <StyledView>
-                                <StyledText className="text-gray-800">Email</StyledText>
-                                <StyledTextInput
-                                    className="rounded-md border border-gray-300 px-4 py-2 text-gray-800"
-                                    onChangeText={onChange}
-                                    onBlur={onBlur}
-                                    value={value}
-                                    keyboardType="email-address"
-                                    autoCapitalize="none"
-                                />
-                                {errors.email && (
-                                    <StyledText className="text-red-500">
-                                        {errors.email.message}
-                                    </StyledText>
-                                )}
-                            </StyledView>
-                        )}
-                    />
-                    <Controller
-                        control={control}
-                        name="password"
-                        render={({ field: { onChange, onBlur, value } }) => (
-                            <StyledView className="mt-4">
-                                <StyledText className="text-gray-800">Password</StyledText>
-                                <StyledTextInput
-                                    className="rounded-md border border-gray-300 px-4 py-2 text-gray-800"
-                                    onChangeText={onChange}
-                                    onBlur={onBlur}
-                                    value={value}
-                                    secureTextEntry
-                                />
-                                {errors.password && (
-                                    <StyledText className="text-red-500">
-                                        {errors.password.message}
-                                    </StyledText>
-                                )}
-                            </StyledView>
-                        )}
-                    />
-                </StyledView>
+            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+                <View className="flex h-full w-full flex-col items-center gap-5">
+                    <View className="mt-32 flex w-full flex-col gap-y-4 px-4">
+                        <Controller
+                            control={control}
+                            name="email"
+                            render={({ field: { onChange, onBlur, value } }) => (
+                                <View>
+                                    <Text className="text-gray-800">Email</Text>
+                                    <TextInput
+                                        className="rounded-md border border-gray-300 px-4 py-2 text-gray-800"
+                                        onChangeText={onChange}
+                                        onBlur={onBlur}
+                                        value={value}
+                                        keyboardType="email-address"
+                                        autoCapitalize="none"
+                                        blurOnSubmit={false}
+                                        returnKeyType="next"
+                                    />
+                                    {errors.email && (
+                                        <Text className="text-red-500">{errors.email.message}</Text>
+                                    )}
+                                </View>
+                            )}
+                        />
+                        <Controller
+                            control={control}
+                            name="password"
+                            render={({ field: { onChange, onBlur, value } }) => (
+                                <View className="mt-4">
+                                    <Text className="text-gray-800">Password</Text>
+                                    <TextInput
+                                        className="rounded-md border border-gray-300 px-4 py-2 text-gray-800"
+                                        onChangeText={onChange}
+                                        onBlur={onBlur}
+                                        value={value}
+                                        secureTextEntry
+                                        blurOnSubmit={true}
+                                        returnKeyType="done"
+                                    />
+                                    {errors.password && (
+                                        <Text className="text-red-500">
+                                            {errors.password.message}
+                                        </Text>
+                                    )}
+                                </View>
+                            )}
+                        />
+                    </View>
 
-                <StyledTouchableOpacity
-                    className="mx-5 mt-4 w-[90%] rounded-md bg-green-600 px-4 py-2 hover:bg-green-700"
-                    disabled={loading}
-                    onPress={handleSubmit(onSubmit)}
-                >
-                    <StyledText className="text-center font-bold text-white">
-                        {loading ? 'Loading...' : 'Login'}
-                    </StyledText>
-                </StyledTouchableOpacity>
-            </StyledView>
+                    <TouchableOpacity
+                        className="mx-5 mt-4 w-[90%] rounded-md bg-green-600 px-4 py-2 hover:bg-green-700"
+                        disabled={loading}
+                        onPress={handleSubmit(onSubmit)}
+                    >
+                        <Text className="text-center font-bold text-white">
+                            {loading ? 'Loading...' : 'Login'}
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </TouchableWithoutFeedback>
         </>
     );
 };
